36f8cdd6e9677d6637785ea2934afbb156a1ebb0
jnoorul <noorul.it@gmail.com>
2019-05-17 03:51:24 +0800

    remove output chunk from search

--- ecommerce/products/src/index.js
+++ ecommerce/products/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Products extends React.Component {
+    constructor(props) {
+        super(props);
+        const shadowRoot = this.attachShadow({mode: 'open'});
+        const appRoot = document.createElement('div');
+        shadowRoot.appendChild(appRoot);
+        ReactDOM.render(<App />, appRoot);
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+customElements.define('amaze-products', Products);
