82a76d10ca0882ff4bf3100271cde0d292444339
jnoorul <noorul.it@gmail.com>
2019-05-17 04:31:51 +0800

    integrating recommendations with home

--- ecommerce/recommendations/src/index.js
+++ ecommerce/recommendations/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Recommendations extends HTMLElement {
+    constructor() {
+        super();
+        const shadowRoot = this.attachShadow({mode: 'open'});
+        const appRoot = document.createElement('div');
+        shadowRoot.appendChild(appRoot);
+        ReactDOM.render(<App />, appRoot);
+    }
+}
+
+customElements.define('amaze-recommendations', Recommendations);
