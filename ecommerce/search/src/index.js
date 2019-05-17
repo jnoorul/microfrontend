52d849f86b72fa664ab1f080900139594130b6c2
jnoorul <noorul.it@gmail.com>
2019-05-17 16:42:26 +0800

    moving style inside shadow dom for search

--- ecommerce/search/src/index.js
+++ ecommerce/search/src/index.js
@@ -7,7 +7,14 @@ class Search extends HTMLElement {
     constructor() {
         super();
         const shadowRoot = this.attachShadow({mode: 'open'});
+
         const appRoot = document.createElement('div');
+
+        const styleTag = document.createElement('link');
+        styleTag.href = 'http://localhost:4001/static/css/app.css';
+        styleTag.rel = 'stylesheet';
+
+        shadowRoot.appendChild(styleTag);
         shadowRoot.appendChild(appRoot);
         ReactDOM.render(<App />, appRoot);
     }
