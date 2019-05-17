4902a79ff97861273c9d2513a314f96c467e47cc
jnoorul <noorul.it@gmail.com>
2019-05-18 00:42:26 +0800

    recommendations - moving styles inside shadow dom

--- ecommerce/recommendations/src/index.js
+++ ecommerce/recommendations/src/index.js
@@ -7,9 +7,16 @@ import App from './App';
 class Recommendations extends HTMLElement {
     constructor() {
         super();
+
         const shadowRoot = this.attachShadow({mode: 'open'});
         const appRoot = document.createElement('div');
+        const styleTag = document.createElement('link');
+        styleTag.href = 'http://localhost:4003/static/css/app.css';
+        styleTag.rel = 'stylesheet';
+
         shadowRoot.appendChild(appRoot);
+        shadowRoot.appendChild(styleTag);
+
         ReactDOM.render(<App />, appRoot);
     }
 }
